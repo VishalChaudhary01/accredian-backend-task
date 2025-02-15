1eaa64089e99b1170d0cef668855a61ab2ebba4c
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-02-15 16:40:01 +0530

    update .sql file

--- prisma/migrations/20250215051954_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[referralCode]` on the table `Referral` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Referral_referralCode_key` ON `Referral`(`referralCode`);
